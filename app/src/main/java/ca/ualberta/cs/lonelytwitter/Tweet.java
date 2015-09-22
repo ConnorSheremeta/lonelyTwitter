398cbfa136a9c4da36de111bd76e3329be91c623
Connor Sheremeta <sheremet@cs.ualberta.ca>
2015-09-21 19:03:09 -0600

    tweets now save

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -42,4 +42,13 @@ public abstract class Tweet extends Object implements Tweetable {
 
     public abstract Boolean isImportant();
 
+    @Override
+    public String toString() {
+        return date.toString() + " | " + text;
+
+
+    }
+
 }
+
+
